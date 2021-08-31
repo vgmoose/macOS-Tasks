5ae0c5e6ae40ba9893a5a8e920c24eac48a06e20
vgmoose <me@vgmoose.com>
2021-08-31 16:22:43 -0400

    Initial Commit

---
+++ tasks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tasks
+//
+//  Created by Ricky on 8/31/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tasks/tasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  tasksApp.swift
+//  tasks
+//
+//  Created by Ricky on 8/31/21.
+//
+
+import SwiftUI
+
+@main
+struct tasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
